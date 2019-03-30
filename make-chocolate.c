fc266c3f3cd7e1aa2a68c6295575162b5b304125
Chrish0r <49090779+Chrish0r@users.noreply.github.com>
2019-03-30 18:35:39 +0100

    Add files via upload

---
+++ make-chocolate.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int small_bars;
+    int big_bars;
+    int goal;
+
+  /* Number of small bars used, -1 means it cannot be done. */
+  int used_small_bars = -1;
+
+  scanf("%d %d %d", &small_bars, &big_bars, &goal);
+
+  if (goal <= (big_bars * 5 + small_bars) && goal >= (big_bars * 5)) {
+
+      small_bars = goal % (big_bars * 5);
+      printf("%d", small_bars);
+      }
+    else if (goal <= (big_bars * 5 + small_bars) && goal < (big_bars * 5)) {
+      small_bars = goal % 5;
+      printf("%d", small_bars);
+    } else {
+         printf("%d", used_small_bars);
+    }
+
+
+    return 0;
+}
