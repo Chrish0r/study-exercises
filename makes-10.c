80b4025991026245b71bc0ba0b481a3159937a69
Chrish0r <49090779+Chrish0r@users.noreply.github.com>
2019-03-31 16:56:01 +0200

    Add files via upload

---
+++ makes-10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a == 10 || b == 10 || a + b == 10) {
+        printf("Makes 10!");
+    }
+
+    return 0;
+}
