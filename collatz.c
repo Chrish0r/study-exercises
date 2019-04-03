3723503a3460de060fce907f4cdb162f7bc15d83
Chrish0r <49090779+Chrish0r@users.noreply.github.com>
2019-04-03 17:41:24 +0200

    Update collatz.c

--- collatz.c
+++ collatz.c
@@ -8,7 +8,7 @@ int main() {
     for (count = 1; n > 1; count++) {
         if (n % 2 == 0) {
             n /= 2;
-		´} else if (n % 2 != 0) {
+	´} else if (n % 2 != 0) {
             n = 3 * n + 1;
         }
     }
