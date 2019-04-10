c0de4c72d98e2e3e1468bed25c87d74ea550a566
Chrish0r <49090779+Chrish0r@users.noreply.github.com>
2019-04-10 17:41:23 +0200

    Add files via upload

---
+++ parrot_trouble-With-Logical-Operators_ALL-in-One.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+  Returns 1 if we are in trouble, and 0 otherwise.
+  If talking == 1, the parrot is talking, otherwise it is silent.
+  The hour is the current hour time in range 0..23.
+*/
+int parrot_trouble(int talking, int hour) {
+    if ((talking == 1) && (hour < 7 || hour > 20)) {
+            return 1;
+    }
+    return 0;
+}
+
+int main(void) {
+  int input_talking;
+  int input_hour;
+
+  scanf("%d %d", &input_talking, &input_hour);
+
+  if (parrot_trouble(input_talking, input_hour))
+    printf("Oh no, parrot in trouble!");
+  else
+    printf("Everything's fine.");
+
+  return 0;
+}
