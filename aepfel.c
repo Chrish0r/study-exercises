92c41fddd866127a9f65645047b63b1ebde49bd5
Chrish0r <49090779+Chrish0r@users.noreply.github.com>
2019-04-15 10:03:57 +0200

    Update aepfel.c

--- aepfel.c
+++ aepfel.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int i, aepfel, fritz = 5, hans = 3;
+    int i, aepfel, fritz = 5, hans = 3, fritz_gesamt = 0, hans_gesamt = 0;
     printf("Wieviele Aepfel kaufen Hans und Fritz?\n");
     scanf("%d", &aepfel);
 
     if (aepfel % 8 == 0) {
         for (i = 1; aepfel > 0; i++) {
             aepfel = aepfel - (fritz + hans);
-            printf("%s %d %12s %d %14s %d\n", "Fritz:", fritz, "Hans:", hans, "Aepfel:", aepfel);
+            fritz_gesamt += fritz;
+            hans_gesamt += hans;
+            printf("%s %5d | %s %5d | %s %5d\n", "Fritz:", fritz_gesamt, "Hans:", hans_gesamt, "Aepfel:", aepfel);
         }
     }
     return 0;
