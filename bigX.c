927b0c80e69a98f0cb03486fc9cef7d6d406dc9c
Chrish0r <49090779+Chrish0r@users.noreply.github.com>
2019-04-23 16:51:56 +0200

    Add files via upload

---
+++ bigX.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int z, s, leer_li, leer_re, n;
+    printf("Please enter the number of rows: ");
+    scanf("%d", &n);
+
+    if (n % 2 != 0) {
+            for (z = 1; z <= n/2; z++) {
+                printf("%*s", z - 1, "");
+                printf("*");
+                for (s = 1; s <=1; s++) {
+                    printf("%*s" , n - 2*z, "");
+                    printf("*\n");
+                }
+            }
+                printf("%*s", z - 1, "");
+                printf("*\n");
+
+                for (z = n/2; z >= 1; z--) {
+                    printf("%*s", z - 1, "");
+                    printf("*");
+                    for (s = 1; s <=1; s++) {
+                        printf("%*s" , n - 2*z, "");
+                        printf("*\n");
+                    }
+                }
+            } else {
+                for (z = 1; z < n/2; z++) {
+                    for (leer_li = 1; leer_li <= 1; leer_li++) {
+                        printf("%*s", z - 1, "");
+                        printf("*");
+                    }
+                    for (leer_re = 1; leer_re <= 1; leer_re++) {
+                        printf("%*s", n - 2*z, "");
+                        printf("*\n");
+                    }
+                }
+
+                for (leer_li = 1; leer_li <= 2; leer_li++) {
+                    printf("%*s", z - 1, "");
+                    printf("**\n");
+                }
+
+                for (leer_li = n/2 - 2; leer_li >= 0; leer_li--) {
+                    printf("%*s", leer_li, "");
+                    printf("*");
+                    for (leer_re = 1; leer_re <= 1; leer_re++) {
+                        printf("%*s", (n - 2) - 2 * leer_li, "");
+                        printf("*\n");
+                    }
+
+                }
+
+            }
+
+   return 0;
+}
