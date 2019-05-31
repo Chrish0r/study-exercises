a6f34068b194b52458bdc7187b28fea959dc9f94
Chrish0r <49090779+Chrish0r@users.noreply.github.com>
2019-05-31 13:27:25 +0200

    Add files via upload

---
+++ fibonacci_iterativ.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int fibonacci(int x) {
+    int i, erster_Summand = 0, zweiter_Summand = 1, summe;
+    if(x == 0) { // statt mit else if, auch einfach zwei separate if und dann for-loop moeglich
+        return 1;
+    }
+    else if(x == 1) {
+        return 1;
+    } else {
+        for(i = 2; i <= x; i++) {
+            summe = erster_Summand + zweiter_Summand;
+            erster_Summand = zweiter_Summand;
+            zweiter_Summand = summe;
+        }
+        return summe;
+    }
+}
+
+int main() {
+    unsigned int n;
+    printf("Bitte geben sie ihre gewuenschte Anzahl an Fibonacci-Folgen ein: \n");
+    scanf("%d", &n);
+
+    printf("Das Ergebnis von fibonacci(%d) lautet: %d\n", n, fibonacci(n));
+
+    return 0;
+}
